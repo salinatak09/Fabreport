74e8c2100fe9f673f0c4c8c6e61590edd9ae08fa
Salina <salinatak2002@gmail.com>
2025-01-20 22:39:00 +0530

    trying to fix DB url

--- src/db/database.ts
+++ src/db/database.ts
@@ -5,7 +5,7 @@ import {connect} from "mongoose";
 let isConnected = (global as any)?.isConnected;
 
 export async function connectToDataBase() {
-  const mongoURL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@fabreport.wgptk.mongodb.net/?retryWrites=true&w=majority&appName=fabreport`;
+  const mongoURL = `mongodb+srv://${process.env.NEXT_PUBLIC_DB_USERNAME}:${process.env.NEXT_PUBLIC_DB_PASSWORD}@fabreport.wgptk.mongodb.net/?retryWrites=true&w=majority&appName=fabreport`;
   console.log({mongoURL});
   // If a cached connection exists, return it
   if (isConnected) {
