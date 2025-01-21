35708c7efc1ac4529a2bbfc5df8b4ef97b439610
Salina <salinatak2002@gmail.com>
2025-01-21 12:31:21 +0530

    trying to fix errors

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -6,14 +6,14 @@ export async function POST(request: Request) {
   try {
     const { name, email, password } = await request.json();
     console.log('payload', { name, email, password });
-    const existUser = await User.findOne({email});
-    console.log({existUser});
-    if(existUser){
-      return NextResponse.json({
-        message: 'User Exists Already!!',
-        error: 'Try with a new Email !'
-      });
-    }
+    // const existUser = await User.findOne({email});
+    // console.log({existUser});
+    // if(existUser){
+    //   return NextResponse.json({
+    //     message: 'User Exists Already!!',
+    //     error: 'Try with a new Email !'
+    //   });
+    // }
     const hashedPassword = await hash(password, 10);
     console.log({hashedPassword});
     const newUser = await User.create({ name, email, password:hashedPassword});
